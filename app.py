f45c0a780747a55ca76c03acd8d6cd2d839da585
Fernando Vinicius <99344303+fvsteinbach@users.noreply.github.com>
2022-08-17 00:17:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run()
